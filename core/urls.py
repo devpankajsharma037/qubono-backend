c3aadfa1627c91e8832c8bfc6d28c620ecc06866
vishal kumar <vishalkumar049022@gmail.com>
2025-09-29 16:29:40 +0530

    change the admin title

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "Qubono"
+admin.site.site_title = "Qubono"
+admin.site.index_title = "Qubono"
 
 
 urlpatterns = [
